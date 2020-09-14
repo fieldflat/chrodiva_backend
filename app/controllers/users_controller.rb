9e35953f9ac422bdbf55c02d424228195d69ebe6
Tomonori HIRATA <hirata@sqlab.jp>
2020-09-15 00:29:42 +0900

    update: create user controller and cors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  def create
+    @user = User.create(user: params[:user])
+    render json: @user
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update_attributes(user: params[:user])
+    render json: @user
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user.destroy
+      head :no_content, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
 end
